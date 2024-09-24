2b16e53a6a1e38bf7be52214172f8d43e245666d
zox2m <zox2mm@gmail.com>
2024-09-24 19:59:58 +0900

    coTe week 1

---
+++ w1/1.cpp
@@ -0,0 +1,16 @@
+// 두 수의 합 
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    int answer = num1 + num2;
+    return answer;
+}
+
+int main(){
+    solution(1,2);
+    return 0;
+}
